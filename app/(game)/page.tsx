2ef2594cc594ac7a993559d35bbcb586e620e266
Phil Zona <phil.b.zona@gmail.com>
2024-01-02 17:07:11 -0500

    Add basic question value buttons to game (not functional)

--- app/(game)/page.tsx
+++ app/(game)/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import PlayerPanel from './components/player-panel';
+import QuestionValueButtons from './components/question-value-buttons';
 
 const GamePage = () => {
   return (
     <main className="w-full lg:w-[1000px] xl:w-[90%] mx-auto">
-      <div className="flex flex-col lg:flex-row justify-between items-center mt-4 lg:mt-12 p-2 sm:p-0">
+      <div className="text-center mx-auto mt-4 lg:mt-8">
+        <QuestionValueButtons />
+      </div>
+      <div className="flex flex-col lg:flex-row justify-between items-center mt-4 lg:mt-8 p-2 sm:p-0">
         <PlayerPanel />
         <PlayerPanel />
         <PlayerPanel />
