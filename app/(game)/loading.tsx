c26e5a5d6dd7e18a349a3c4324924cf69e233f33
Phil Zona <phil.b.zona@gmail.com>
2024-01-19 00:04:18 -0500

    Add loading page

---
+++ app/(game)/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+  return (
+    <main className="w-full flex flex-col items-center justify-center">
+      <div className="loading loading-spinner loading-lg text-blue mt-24" />
+    </main>
+  );
+};
+
+export default LoadingPage;
