bb72850af94897b51050b0ef5de0cb0d451f3f78
Phil Zona <phil.b.zona@gmail.com>
2024-01-19 16:19:34 -0500

    Add player name change feature
    Update button hover

--- app/(game)/_components/player/player-name-modal.tsx
+++ app/(game)/_components/player/player-name-modal.tsx
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useGameStore } from '@/app/(game)/_store/game-store';
 
 type PlayerNameModalProps = {
   playerIndex: number;
 };
 
 export const PlayerNameModal = ({ playerIndex }: PlayerNameModalProps) => {
+  const playerName = useGameStore.use.players()[playerIndex].name;
+  const setPlayerName = useGameStore.use.setPlayerName();
+
+  const [inputName, setInputName] = useState<string>(playerName);
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  const handleNameChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setInputName(e.currentTarget.value);
+  };
+
+  const handleCancel = () => {
+    setInputName(playerName);
+    modalRef?.current?.close();
+  };
+
+  const handleSubmit = () => {
+    setPlayerName(playerIndex.toString(), inputName);
+  };
+
   return (
     <dialog
       id={`player-name-${playerIndex}`}
       className="modal"
+      ref={modalRef}
     >
       <div className="modal-box">
-        <h3 className="font-bold text-lg">Hello!</h3>
-        <p className="py-4">Press ESC key or click the button below to close</p>
+        <h3 className="font-bold text-md text-black">Edit player name</h3>
         <div className="modal-action">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button
-              className="btn"
-              type="submit"
-            >
-              Close
-            </button>
+          <form
+            method="dialog"
+            className="flex flex-col justify-center w-full"
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-full mb-4"
+              value={inputName}
+              onChange={handleNameChange}
+            />
+
+            <div className="flex space-x-4 justify-end">
+              <button
+                className="btn"
+                type="button"
+                onClick={handleCancel}
+              >
+                Cancel
+              </button>
+              <button
+                className="btn bg-blue text-white hover:bg-blue-light"
+                type="submit"
+              >
+                Save
+              </button>
+            </div>
           </form>
         </div>
       </div>
