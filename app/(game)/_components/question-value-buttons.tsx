a95fa312519bf8128a29f84c77a21b9c951b1d87
Phil Zona <phil.b.zona@gmail.com>
2024-01-19 23:53:35 -0500

    Update question value buttons to account for round

--- app/(game)/_components/question-value-buttons.tsx
+++ app/(game)/_components/question-value-buttons.tsx
@@ -4,35 +4,36 @@ import React from 'react';
 import clsx from 'clsx';
 import { useGameStore } from '../_store/game-store';
 
-const VALUES = [200, 400, 600, 800, 1000];
-
 const QuestionValueButtons = () => {
-  const activeValue = useGameStore(state => state.activeValue);
-  const setActiveValue = useGameStore(state => state.setActiveValue);
-  const scoreValues = useGameStore(state => state.scoreValues);
+  const activeValue = useGameStore.use.activeValue();
+  const setActiveValue = useGameStore.use.setActiveValue();
+  const scoreValues = useGameStore.use.scoreValues();
+  const round = useGameStore.use.round();
 
   const handleValueButtonClick = (val: number) => {
     setActiveValue(val);
   };
 
   return (
-    <div className="flex justify-center items-center gap-3">
-      {scoreValues.map(value => {
-        const isActive = activeValue === value;
-        return (
-          <button
-            className={clsx(
-              'text-yellow text-lg font-bold hover:bg-gradient-to-b from-red to-purple px-4 py-2 rounded-md scale-100 hover:scale-105',
-              isActive ? 'bg-gradient-to-b scale-105' : 'bg-blue'
-            )}
-            key={value}
-            onClick={() => handleValueButtonClick(value)}
-            type="button"
-          >
-            ${value}
-          </button>
-        );
-      })}
+    <div className="flex justify-center items-center gap-3 max-w-full">
+      {scoreValues
+        .map(value => value * round) // Handle this in state middleware or something later
+        .map(value => {
+          const isActive = activeValue === value;
+          return (
+            <button
+              className={clsx(
+                'text-yellow text-md sm:text-lg font-bold hover:bg-gradient-to-b from-red to-purple px-4 py-2 rounded-md scale-100 hover:scale-105',
+                isActive ? 'bg-gradient-to-b scale-105' : 'bg-blue'
+              )}
+              key={value}
+              onClick={() => handleValueButtonClick(value)}
+              type="button"
+            >
+              ${value}
+            </button>
+          );
+        })}
     </div>
   );
 };
